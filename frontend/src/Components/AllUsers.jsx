6948b9bfed00179adfc94cbf518e873dc55fdb1e
ab-h-i-n <abhinshelly@gmail.com>
2024-03-30 09:37:45 +0530

    .

--- frontend/src/Components/AllUsers.jsx
+++ frontend/src/Components/AllUsers.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AllUsers = () => {
-  return <div className="h-[85vh] bg-blue-600 w-[100vw]">AllUsers</div>;
+  return <div className="h-[82vh] bg-blue-600 w-[100vw]">AllUsers</div>;
 };
 
 export default AllUsers;
