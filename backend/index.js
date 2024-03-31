23e1bb57cc997c7031f24344aae452ace000c01e
ab-h-i-n <abhinshelly@gmail.com>
2024-03-31 22:04:20 +0530

    Update backend index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
+import mongoose, { connect } from 'mongoose';
 import dotenv from 'dotenv';
 import router from './routes.js';
 import { Server } from 'socket.io';
@@ -13,12 +13,16 @@ const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+//middlewar
+
 const uri = process.env.MONGODB_URI;
 const io = new Server(server, {
   cors: {
-    origin: 'https://chat-app-frontend-hazel.vercel.app'
+    origin: '*'
   }
 });
+
+// mongodb 
 
 mongoose
   .connect(uri)
@@ -27,7 +31,10 @@ mongoose
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error.message);
-  });
+  });https://chat-app-frontend-hazel.vercel.app
+
+
+//   socket connect 
 
 io.on("connect", async (socket) => {
   const userId = socket.handshake.auth.token;
@@ -40,8 +47,10 @@ io.on("connect", async (socket) => {
   });
 });
 
+// app use 
+
 app.use(express.json({ limit: '10mb' }));
 app.use(cors({
-  origin: 'https://chat-app-frontend-hazel.vercel.app'
+  origin: '*'
 }));
 app.use(router);
