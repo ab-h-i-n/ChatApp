a277a5f5be166071e6f93d3206f533db49181bed
ab-h-i-n <abhinshelly@gmail.com>
2024-03-31 21:59:38 +0530

    Update backend/index.js with socket.io configuration and user status handling

--- backend/index.js
+++ backend/index.js
@@ -3,51 +3,45 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import router from './routes.js';
-import { Server } from 'socket.io'; // Use Server instead of Socket
+import { Server } from 'socket.io';
 import User from './UserSchema.js';
 
 const app = express();
-
-app.use(cors());
-
-app.use(express.json({ limit: '10mb' }));
-app.use(router);
 dotenv.config();
-
 const PORT = process.env.PORT;
-
 const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 const uri = process.env.MONGODB_URI;
-
 const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-})
+  cors: {
+    origin: 'https://chat-app-frontend-hazel.vercel.app'
+  }
+});
 
-
-mongoose.connect(uri)
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error.message);
-    });
-
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
 
 io.on("connect", async (socket) => {
+  const userId = socket.handshake.auth.token;
+  await User.findOneAndUpdate({ _id: userId }, { $set: { onlineStatus: true } });
+  io.emit("userUpdate");
 
-    const userId = socket.handshake.auth.token;
+  socket.on("disconnect", async () => {
+    await User.findOneAndUpdate({ _id: userId }, { $set: { onlineStatus: false } });
+    io.emit("userUpdate");
+  });
+});
 
-    await User.findOneAndUpdate({ _id: userId }, { $set: { onlineStatus: true } })
-
-    io.emit("userUpdate")
-
-    socket.on("disconnect", async () => {
-        await User.findOneAndUpdate({ _id: userId }, { $set: { onlineStatus: false } })
-        io.emit("userUpdate")
-    });
-})
+app.use(express.json({ limit: '10mb' }));
+app.use(cors({
+  origin: 'https://chat-app-frontend-hazel.vercel.app'
+}));
+app.use(router);
